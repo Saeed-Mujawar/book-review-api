c05e2a092d50f16927402e2b1752179f2dfc2801
Saeed-Mujawar <saeedmujawar20@gmail.com>
2024-10-01 19:50:06 +0530

    commiited

--- src/middleware.py
+++ src/middleware.py
@@ -34,5 +34,5 @@ def register_middleware(app: FastAPI):
 
     app.add_middleware(
         TrustedHostMiddleware,
-        allowed_hosts=["localhost", "127.0.0.1" ,"bookly-api-dc03.onrender.com","0.0.0.0"],
+        allowed_hosts=["localhost", "127.0.0.1" ,"book-review-api-k7hl.onrender.com","0.0.0.0"],
     )
